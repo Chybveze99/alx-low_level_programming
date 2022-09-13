00aee7011f5a624105033044715d9b8fd06496c8
Chybveze99 <goodychiby55@gmail.com>
2022-09-12 20:39:10 -0700

    Program prints using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#!/bin/bash
-   /**
-    *Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line using the function puts and end with value 0
-    */
+#include <stdio.h>
+
+/**
+* This program print "Programming is like building a multilingual puzzle" using the function puts
+* Returns 0
+*/
 int main(void)
 {
-  char str[] = "Programming is like building a multilingual puzzle";
-  puts(str);
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
